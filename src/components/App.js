86e636e9a5d2b3487dea11d49718c3b4d8acbdfb
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2022-05-10 23:57:13 +0300

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
